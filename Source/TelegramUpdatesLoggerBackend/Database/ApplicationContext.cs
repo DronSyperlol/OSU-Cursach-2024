0d2b255e00afc31bdd66f4dc068e641adc22782c
Dron <115934404+DronSyperlol@users.noreply.github.com>
2024-12-11 20:23:23 +0500

    add: docker & rename backend

---
+++ Source/TelegramUpdatesLoggerBackend/Database/ApplicationContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Database
+{
+    internal class ApplicationContext : DbContext
+    {
+    }
+}
---
+++ Source/TelegramUpdatesLoggerBackend/Database/Entities/Users.cs
@@ -0,0 +1,20 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Database.Entities
+{
+    internal class Users
+    {
+        public long Id { get; set; } // Вместо AUTO_INCREMENT будут айдишники из телеграма.
+        [MaxLength(64)]
+        public string FirstName { get; set; }
+        [MaxLength(64)]
+        public string? LastName { get; set; }
+        [MaxLength(32)]
+        public string? Username { get; set; }
+        [MaxLength(10)]
+        public string LanguageCode {  get; set; }
+
+    }
+}
