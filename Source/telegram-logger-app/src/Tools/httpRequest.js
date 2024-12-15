0e64819aa85ad881c5afb34bac945853cabfb46d
Dron <115934404+DronSyperlol@users.noreply.github.com>
2024-12-15 21:56:17 +0500

    fix: workflow build

--- Source/telegram-logger-app/src/Tools/httpRequest.js
+++ Source/telegram-logger-app/src/Tools/httpRequest.js
@@ -1,22 +1,24 @@
 export async function http_get(url, params = undefined) {
     return new Promise((resolve, reject) => {
         const xml = new XMLHttpRequest();
-        xml.onreadystatechange = function (response) {
-            if (this.readyState == 4 && this.status == 200) {
-                resolve(xml.responseText);
-            } 
-            else {
-                reject(this);
+        xml.onreadystatechange = function () {
+            if (this.readyState === XMLHttpRequest.DONE) {
+                if (this.status === 200) {
+                    resolve(xml.responseText);
+                } 
+                else {
+                    reject(this);
+                }
             }
+            if (params !== undefined) {
+                var strParams = new URLSearchParams(params).toString(); 
+                xml.open("GET", url + '?' + strParams);
+            }
+            else {
+                xml.open("GET", url, true);
+            }
+            xml.send(null);
         }
-        if (params != undefined) {
-            var strParams = new URLSearchParams(params).toString(); 
-            xml.open("GET", url + '?' + strParams);
-        }
-        else {
-            xml.open("GET", url, true);
-        }
-        xml.send(null);
     });
 }
 
@@ -25,8 +27,8 @@ export async function http_post(url, params = undefined) {
     return new Promise((resolve, reject) => {
         const xml = new XMLHttpRequest();
         xml.onreadystatechange = function (response) {
-            if (this.readyState == XMLHttpRequest.DONE) {
-                if (this.status == 200)
+            if (this.readyState === XMLHttpRequest.DONE) {
+                if (this.status === 200)
                     resolve(xml.responseText);
                 else 
                     reject(this);
