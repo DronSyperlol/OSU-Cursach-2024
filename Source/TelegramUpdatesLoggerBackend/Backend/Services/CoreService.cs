b0e618a2dedae56acbb4a3cf77af607157cc62ba
Dron <115934404+DronSyperlol@users.noreply.github.com>
2025-04-24 18:54:42 +0500

    add(Docs): алгоритмы

--- Source/TelegramUpdatesLoggerBackend/Backend/Services/CoreService.cs
+++ Source/TelegramUpdatesLoggerBackend/Backend/Services/CoreService.cs
@@ -1,14 +1,7 @@
-﻿
-using Database;
-using Core.Interfaces;
-using System.Reflection;
-
-namespace Backend.Services
+﻿namespace Backend.Services
 {
     public class CoreService : BackgroundService
     {
-        static readonly PeriodicTimer _timer = new(TimeSpan.FromSeconds(1));
-
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
             => await Core.Services.CoreService.Instance.ExecuteAsync(stoppingToken);
     }
